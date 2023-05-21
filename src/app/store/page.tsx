bfaeb40e4ddbd98a249a31a5393363205c908345
Eric <eriliu@ucdavis.edu>
2023-05-21 09:33:27 -0700

    Fix image for store page

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,27 +1,18 @@
 import { Account } from 'appwrite'
-import { faFire } from '@fortawesome/free-solid-svg-icons'
+import { faFire, faIceCream } from '@fortawesome/free-solid-svg-icons'
 import Api from '@/lib/api'
 import Icon from '@/components/icon'
 import Navbar from '@/components/navbar'
+import Image from 'next/image'
+import bg from './night.jpg'
 
 export default function Home(): JSX.Element {
 	return (
 		<>
 			<Navbar />
-			<div className='flex flex-col items-center justify-center h-screen space-y-10'>
-				<div className='card card-side bg-base-100 shadow-xl w-5/12'>
-					<figure><img src='/images/stock/photo-1635805737707-575885ab0820.jpg' alt='Movie' /></figure>
-					<div className='card-body'>
-						<h2 className='card-title'>Streak Freeze</h2>
-						<p>This will freeze your streak so that it will be safe for the next 2 days. </p>
-						<div className='card-actions justify-end'>
-							<button className='btn btn-primary'>Buy</button>
-						</div>
-					</div>
-				</div>
-
-				<div className='card card-side bg-base-100 shadow-xl w-5/12'>
-					<figure><img src='/images/stock/photo-1635805737707-575885ab0820.jpg' alt='Movie' /></figure>
+			<div className='flex flex-col items-center justify-center h-max pt-10 space-y-10'>
+				<div className='card card-side bg-base-100 shadow-xl items-center w-5/12'>
+					<figure><Image src={bg} alt='Night' width='130' height='130' /></figure>
 					<div className='card-body'>
 						<h2 className='card-title'>Streaky Sleep</h2>
 						<p>Add 2 to your current Streak and keep the sleep going (only valid once).</p>
@@ -32,7 +23,18 @@ export default function Home(): JSX.Element {
 				</div>
 
 				<div className='card card-side bg-base-100 shadow-xl w-5/12'>
-					<figure><img src='/images/stock/photo-1635805737707-575885ab0820.jpg' alt='Movie' /></figure>
+					<figure><Image src={bg} alt='Night' width='130' height='130' /></figure>
+					<div className='card-body'>
+						<h2 className='card-title'>Streaky Sleep</h2>
+						<p>Add 2 to your current Streak and keep the sleep going (only valid once).</p>
+						<div className='card-actions justify-end'>
+							<button className='btn btn-primary'>Buy</button>
+						</div>
+					</div>
+				</div>
+
+				<div className='card card-side bg-base-100 shadow-xl w-5/12'>
+					<figure><Image src={bg} alt='Night' width='130' height='130' /></figure>
 					<div className='card-body'>
 						<h2 className='card-title'>Double Or Nothing</h2>
 						<p>Wager your points, but make double if you meet your 3 next daily goals.</p>
