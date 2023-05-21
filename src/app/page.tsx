707d183d63c60278f4dce941c2dac951e7396b37
Eric <eriliu@ucdavis.edu>
2023-05-20 17:28:03 -0700

    homepage: Make button autologin if detected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+'use client'
+
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import Icon from '@/components/icon'
-import Header from '@/components/header'
-import Footer from '@/components/footer'
+import Api from '@/lib/api'
+
+async function start() {
+	try {
+		await Api.account.get()
+		location.replace('/home')
+	}
+	catch (err) {
+		location.replace('/login')
+	}
+}
 
 export default function Index() {
 	return (<>
 		<div className='flex flex-col justify-center items-center gap-y-5 h-screen'>
 			<h1 className='text-8xl font-semibold text-yellow-300'>Drowsy <Icon icon={faStar} /></h1>
 			<h2 className='text-2xl'>Make sleeping a habit.</h2>
-			<a className='btn btn-lg mt-10' href='/login'>Get started</a>
+			<button className='btn btn-lg mt-10' onClick={start}>Get started</button>
 		</div>
 	</>
 	)
