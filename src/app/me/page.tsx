ecb49de64b6b868739deb109fcc5b5bc92ab4aaf
Eric <eriliu@ucdavis.edu>
2023-05-20 20:35:30 -0700

    Fix graph on profile

--- src/app/me/page.tsx
+++ src/app/me/page.tsx
@@ -4,6 +4,32 @@ import Api from '@/lib/api'
 import Icon from '@/components/icon'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
+import { Inter } from 'next/font/google'
+import {
+	Chart,
+	PointElement,
+	LinearScale,
+	CategoryScale,
+	LineElement,
+	Tooltip,
+	ChartOptions,
+	ChartData
+} from 'chart.js'
+import { Line } from 'react-chartjs-2'
+
+Chart.register(
+	LineElement,
+	PointElement,
+	LinearScale,
+	CategoryScale,
+	Tooltip
+)
+
+const inter = Inter({ subsets: ['latin'] })
+Chart.defaults.backgroundColor = 'rgba(0, 0, 0, 0)'
+Chart.defaults.borderColor = 'rgba(255, 255, 255, 255)'
+Chart.defaults.font.size = 20
+Chart.defaults.color = '#748aad'
 
 export default function Me(): JSX.Element {
 	async function initUser() {
@@ -19,10 +45,62 @@ export default function Me(): JSX.Element {
 	const [name, setName] = useState('')
 	const [email, setEmail] = useState('')
 
+	const opts: ChartOptions<'line'> = {
+		maintainAspectRatio: false,
+		responsive: false,
+		scales: {
+			x: {
+				grid: {
+					display: false
+				},
+				border: {
+					display: false
+				},
+				ticks: {
+					padding: 20,
+					font: {
+						size: 25
+					}
+				}
+			},
+			y: {
+				grid: {
+					color: '#273449',
+					lineWidth: 3,
+				},
+				border: {
+					display: false
+				},
+				ticks: {
+					padding: 20,
+					maxTicksLimit: 3,
+					font: {
+						size: 25
+					}
+				},
+			}
+		}
+	}
+
+	const data: ChartData<'line'> = {
+		labels: [0, 1, 2, 3],
+		datasets: [
+			{
+				label: 'my data',
+				data: [1, 3, 5, 6],
+				pointBackgroundColor: '#b5cdf5',
+				pointBorderColor: '#b5cdf5',
+				pointRadius: 8,
+				borderWidth: 7,
+				borderColor: '#b5cdf5'
+			}
+		]
+	}
+
 	initUser()
 	return (<>
 		<div className='flex flex-col items-center justify-center h-screen'>
-			<div className='flex flex-col card bg-base-100 content-center w-11/12 space-y-3 py-8 text-center'>
+			<div className='flex flex-col card bg-base-100 content-center w-9/12 space-y-3 py-8 text-center'>
 
 				{ // User info is behind suspense when not loaded
 					name === '' || email === '' ?
@@ -30,10 +108,10 @@ export default function Me(): JSX.Element {
 							<div className='animate-pulse flex flex-col justify-center space-y-6'>
 								<div className='flex items-center justify-center space-x-4'>
 									<div className='h-11 w-11 rounded-full bg-neutral-focus' />
-									<div className='h-8 w-5/12 rounded-full bg-neutral-focus' />
+									<div className='h-8 w-72 rounded-full bg-neutral-focus' />
 								</div>
 								<div className='flex items-center justify-center'>
-									<div className='h-5 w-5/12 rounded-full bg-neutral-focus' />
+									<div className='h-5 w-80 rounded-full bg-neutral-focus' />
 								</div>
 							</div>
 						</>
@@ -42,6 +120,9 @@ export default function Me(): JSX.Element {
 							<h2 className='text-3xl text-neutral-content'>{email}</h2>
 						</>
 				}
+				<div className='flex justify-center'>
+					<Line options={opts} data={data} width={1000 /* This is SO hacky ew */} height={600 /* Same with this */} />
+				</div>
 			</div>
 		</div>
 	</>)
