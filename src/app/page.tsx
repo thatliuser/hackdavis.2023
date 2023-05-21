4cb81224a77a532162ed2b215ac6725f01850a42
Eric <eriliu@ucdavis.edu>
2023-05-21 00:48:53 -0700

    Make the Drowsy star spin and add a catchphrase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,8 +20,8 @@ export default function Index() {
 	}
 	return (<>
 		<div className='flex flex-col justify-center items-center gap-y-5 h-screen'>
-			<h1 className='text-8xl font-semibold text-yellow-300'>Drowsy <Icon icon={faStar} /></h1>
-			<h2 className='text-2xl'>Make sleeping a habit.</h2>
+			<h1 className='text-8xl font-semibold text-yellow-300'>Drowsy <Icon icon={faStar} className='animate-spin-slow' /></h1>
+			<h2 className='text-2xl'>Catch those Zzzs.</h2>
 			<button className='btn btn-lg mt-10' onClick={start}>Get started</button>
 		</div>
 	</>
