cdd7af12da9861123055cbad6053836e9c73fa42
Eric <eriliu@ucdavis.edu>
2023-05-20 22:31:00 -0700

    Try to fix GitHub action again

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+const isGhPages = process.env["GITHUB_ACTION"] ? true : false
+
+// Vercel I hope you know you suck for this and I will never use your services again
 /** @type {import('next').NextConfig} */
-const config = {
-	output: 'export'
+const deployConfig = {
+	output: 'export',
+	basePath: '/hackdavis.2023',
+	assetPrefix: '/hackdavis.2023'
 }
+
+/** @type {import('next').NextConfig} */
+const config = (isGhPages ? deployConfig : {})
 
 export default config
